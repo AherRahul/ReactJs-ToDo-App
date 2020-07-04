6d551672f8db3d90cedb9d1cd871defa4905e30f
aherrahul1995 <43370315+aherrahul1995@users.noreply.github.com>
2020-07-04 22:42:48 +0530

    Final commit

---
+++ build/precache-manifest.c2de75f93b0027da7c05e6f4b5466b58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9e4f810aaee1d216e8d95859596bd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2c95741018d9c1fb17b",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c344f4b28341d2875617",
+    "url": "/static/css/main.f99afa9e.chunk.css"
+  },
+  {
+    "revision": "b2c95741018d9c1fb17b",
+    "url": "/static/js/2.7d0dc8bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d0dc8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c344f4b28341d2875617",
+    "url": "/static/js/main.89f486e8.chunk.js"
+  },
+  {
+    "revision": "d19e813aadc2807b3ea9",
+    "url": "/static/js/runtime-main.3a9c02fd.js"
+  },
+  {
+    "revision": "f1108233f6b385ccc2bca904205ef2d2",
+    "url": "/static/media/BTS 3 Discs - Gradient.f1108233.png"
+  }
+]);
